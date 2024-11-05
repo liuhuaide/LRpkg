361a3599477a39eb1bb373ee666ebe011e0917f5
liuhuaide <jcakcino@gmail.com>
2024-11-05 22:44:13 +0100

    new pred method

--- R/ridgereg_methods.R
+++ R/ridgereg_methods.R
@@ -13,34 +13,63 @@ print.ridgereg <- function(x, ...) {
 }
 
 
-#' Predict Fitted Values from Ridge Regression Model
+#' pred.ridgereg: Generate predictions from a ridge regression model
 #'
-#' This function predicts the fitted values from a ridge regression model. It can handle new data
-#' or use the data originally used to fit the model if no new data is provided. The function
-#' standardizes the predictor variables using the mean and standard deviation from the original data.
+#' This function generates predictions based on a ridge regression model created with `ridgereg`.
+#' It can either use the original data (if `newdata` is not provided) or make predictions
+#' on a new dataset.
 #'
-#' @param object A ridge regression model object returned by the \code{ridgereg} function.
-#' @param newdata A data frame containing the new data for which predictions are to be made.
-#' If \code{NULL} (default), the original data used to fit the model will be used.
-#' @param ... Additional arguments to be passed to methods.
+#' @param object An object of class `ridgereg`, which contains the ridge regression model, 
+#'               the fitted coefficients, and the original data used to build the model.
+#' @param newdata A data frame containing new observations for the predictors. If provided, 
+#'                predictions will be generated for these new observations. If NULL (the default), 
+#'                the function will use the original data from `object`.
+#' @param ... Additional arguments (currently not used).
 #'
-#' @return A numeric vector of fitted values corresponding to the new data.
+#' @return A vector of predicted values. If `newdata` is provided, the predictions are based on 
+#'         this data. Otherwise, the predictions are based on the original data in `object`.
 #' @export
 pred.ridgereg <- function(object, newdata = NULL, ...) {
+  # 获取模型的系数
+  beta_hat <- object$coefficients
+  
+  # 如果没有提供 newdata
   if (is.null(newdata)) {
-    newdata <- object$data
+    # 使用模型数据生成设计矩阵
+    X_new <- model.matrix(object$formula, object$data)
+    
+    # 去掉截距列（假设第一列是截距），然后进行标准化
+    X_new_intercept <- X_new[, -1]
+    X_new_norm <- scale(X_new_intercept,
+                        center = sapply(colnames(X_new_intercept), function(col) mean(object$data[[col]])), 
+                        scale = sapply(colnames(X_new_intercept), function(col) sd(object$data[[col]])))
+    
+    # 重新添加截距列
+    X_new_norm <- cbind(Intercept = 1, X_new_norm)
+    
+  } else {
+    # 如果提供了 newdata，直接使用 newdata 的自变量构建标准化矩阵
+    
+    # 如果没有截距列，则手动添加
+    if (!"(Intercept)" %in% colnames(newdata)) {
+      newdata <- data.frame("(Intercept)" = 1, newdata, check.names = FALSE)
+    }
+    
+    # 提取自变量（忽略第一列截距列），并检查数值型列
+    X_new_intercept <- newdata[, -1, drop = FALSE]
+    numeric_cols <- sapply(X_new_intercept, is.numeric)
+    X_new_numeric <- X_new_intercept[, numeric_cols, drop = FALSE]
+    
+    # 对数值型自变量进行标准化
+    X_new_norm <- scale(X_new_numeric,
+                        center = sapply(names(X_new_numeric), function(col) mean(object$data[[col]], na.rm = TRUE)), 
+                        scale = sapply(names(X_new_numeric), function(col) sd(object$data[[col]], na.rm = TRUE)))
+    
+    # 将截距列与标准化后的数值自变量合并
+    X_new_norm <- cbind(Intercept = 1, X_new_norm)
   }
   
-  beta_hat <- object$coefficients
-  X_new <- model.matrix(object$formula, newdata)
-  
-  X_new_intercept <- X_new[, -1]
-  
-  X_new_norm <- scale(X_new_intercept, center = sapply(colnames(X_new_intercept), function(col) mean(object$data[[col]])), 
-                      scale = sapply(colnames(X_new_intercept), function(col) sd(object$data[[col]])))
-  
-  X_new_norm <- cbind(Intercept = 1, X_new_norm)
-  
+  # 计算并返回拟合值
   fitted_values_new <- X_new_norm %*% beta_hat
   return(fitted_values_new)
 }
